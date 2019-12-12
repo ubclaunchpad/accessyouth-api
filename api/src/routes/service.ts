00ef69c014ab9319fa74ed832b4a5cd23bb86bc9
Alan Zhang <a.b.zhang@outlook.com>
2019-12-12 02:47:12 -0800

    added validator for uuid

--- api/src/routes/service.ts
+++ api/src/routes/service.ts
@@ -1,57 +1,75 @@
-import express, {Router,Request,Response} from 'express';
+import express, { Router, Request, Response, NextFunction } from 'express';
 const router: Router = express.Router();
+
+import validator from 'validator';
 
 const Service = require('../models/service');
 
-router.post('/create', (req: Request, res: Response) => {
-  // TODO: validate req
+router.post('/create', (req: Request, res: Response, next: NextFunction) => {
+  if (!validator.isUUID(req.body.uuid)) {
+    res.status(400).send('Invalid UUID');
+    return;
+  }
+
   Service.create({
     uuid: req.body.uuid,
     name: req.body.name,
     currentLocation: req.body.currentLocation,
     description: req.body.description,
-  }, (err: any, doc: any) => {
+  }, (err: any, service: any) => {
     if (err) {
       res.status(500).send('Internal Error');
     } else {
-      res.status(200).send(doc);
+      res.status(200).send(service);
     }
   });
 });
 
-router.get('/getService', (req: Request, res: Response) => {
-  // TODO: validate req
+router.get('/getService', (req: Request, res: Response, next: NextFunction) => {
+  if (!validator.isUUID(req.body.uuid)) {
+    res.status(400).send('Invalid UUID');
+    return;
+  }
+
   Service.findOne({
     uuid: req.query.uuid
-  }, (err: any, doc: any) => {
+  }, (err: any, service: any) => {
     if (err) {
       res.status(500).send('Internal Error');
     } else {
-      res.status(200).send(doc);
+      res.status(200).send(service);
     }
   });
 });
 
-router.post('/updateLocation', (req: Request, res: Response) => {
-  // TODO: validate req
-  if (!req.body.currentLocation.lat || !req.body.currentLocation.lon){
-    res.status(400).send("Location must have a lat and lon")
-  } else {
-    Service.updateOne({
-      uuid: req.body.uuid
-    }, {
-      $set: {currentLocation: req.body.currentLocation}
-    }, {runValidators: true}, (err: any, doc: any) => {
-      if (err) {
-        res.status(500).send({message: err});
-      } else {
-        res.status(200).send("Location updated successfully");
-      }
-    });
+router.post('/updateLocation', (req: Request, res: Response, next: NextFunction) => {
+  if (!validator.isUUID(req.body.uuid)) {
+    res.status(400).send('Invalid UUID');
+    return;
+  } else if (!req.body.currentLocation.lat || !req.body.currentLocation.lon) {
+    res.status(400).send('currentLocation must have lat and lon');
+    return;
   }
+
+  Service.updateOne({
+    uuid: req.body.uuid
+  }, {
+    $set: { currentLocation: req.body.currentLocation }
+  }, (err: any, service: any) => {
+    if (err) {
+      res.status(500).send({ message: err });
+    } else {
+      res.status(200).send("Location updated successfully");
+    }
+  });
 });
 
-router.post('/getLocation', (req: Request, res: Response) => {
+router.post('/getLocation', (req: Request, res: Response, next: NextFunction) => {
+  if (!validator.isUUID(req.body.uuid)) {
+    res.status(400).send('Invalid UUID');
+    return;
+  }
+
   Service.findOne({
     uuid: req.body.uuid
   }, (err: any, service: any) => {
@@ -65,37 +83,36 @@ router.post('/getLocation', (req: Request, res: Response) => {
   });
 });
 
-router.get('/getAllServices', (req: Request, res: Response) => {
+router.get('/getAllServices', (req: Request, res: Response, next: NextFunction) => {
   Service.find({}, (err: any, services: any) => {
     if (err) {
       res.status(500).send('Internal Error');
-    } else if (!services) {
-      res.status(404).send('No services in database');
     } else {
-       res.status(200).send(services);
+      res.status(200).send(services);
     }
   });
 });
 
-router.post('/updateDetails', async (req: Request, res: Response) => {
+router.post('/updateDetails', async (req: Request, res: Response, next: NextFunction) => {
+  if (!validator.isUUID(req.body.uuid)) {
+    res.status(400).send('Invalid UUID');
+    return;
+  } else if (!req.body.description) {
+    res.status(500).send("Invalid description");
+    return;
+  }
 
-  if (!req.body.description) {
-    res.status(500).send("No description was added!")
-  } else if (req.body.description == null) {
-    res.status(500).send("Cannot have null as description!")
-  } else {
-  // TODO: validate req
   Service.updateOne({
     uuid: req.body.uuid
   }, {
-    $set: {description: req.body.description}
-  }, {runValidators: true}, (err: any,doc: any) => {
+    $set: { description: req.body.description }
+  }, (err: any, service: any) => {
     if (err) {
       res.status(500).send({message: err});
     } else {
       res.status(200).send("Description updated successfully");
     }
   });
-}});
+});
 
 module.exports = router;
